1b67636267783c2ecc8b9827d2896ba9109df15c
miicck <michael.hutcheon@hotmail.co.uk>
2019-01-18 18:37:30 +0000

    Added color picker and opacity

--- main.js
+++ main.js
@@ -6,15 +6,47 @@ var painter = (function(){ // Namespace painter
 	var mousepath;
 	var mouse_down = false;
 	var brush_size = 10;
+	var brush_color = "#000000"
+	var brush_opacity = 0.01;
 
-	function paint(x,y,r,g,b,a){
-		context.fillStyle="rgba("+r+","+g+","+b+","+a+")";
+	bob_ross_colors = {
+		"sap green"        : "#0A3410",
+		"alizarin crimson" : "#4E1500",
+		"van dyke brown"   : "#221B15",
+		"dark sienna"      : "#5F2E1F",
+		"midnight black"   : "#000000",
+		"prussian blue"    : "#021E44",
+		"phthalo blue"     : "#0C0040",
+		"phthalo green"    : "#102E3C",
+		"cadmium yellow"   : "#FFEC00",
+		"yellow ochre"     : "#C79B00",
+		"indian yellow"    : "#FFB800",
+		"bright red"       : "#DB0000",
+		"titanium white"   : "#FFFFFF"
+	};
+	hex_values = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+
+	function float_to_hex(f){
+		v1 = Math.floor(f * 15);
+		v2 = Math.floor((f*15 - v1) * 15);
+		ret = hex_values[v1] + hex_values[v2];
+		return ret;
+	}
+
+	function paint(x,y){
+		if (x < 0) return;
+		if (y < 0) return;
+		if (x > canvas.width) return;
+		if (y > canvas.height) return;
+		context.fillStyle = brush_color + float_to_hex(brush_opacity);
 		context.beginPath();
 		context.arc(x,y,brush_size/2,0,2*Math.PI,false);
 		context.fill();
 	}
 
 	function get_path(from, to){
+		// Returns a path linking two points
+		// on the canva sith a straight line
 		dx = to[0] - from[0];
 		dy = to[1] - from[1];
 
@@ -66,7 +98,7 @@ var painter = (function(){ // Namespace painter
 			p2 = mousepath[mousepath.length-1];
 			path = get_path(p1,p2);
 			for (i in path)
-				paint(path[i][0], path[i][1], 255, 0, 0, 0.01);
+				paint(path[i][0], path[i][1]);
 		}
 	}
 	
@@ -88,13 +120,51 @@ var painter = (function(){ // Namespace painter
 			context.fillStyle = "rgb("+255+","+255+","+255+")";
 			context.fillRect(0,0,512,512);
 
-			canvas.onmousedown = function(event){
-				mouse_down = true;
+			document.body.onmousedown = function(event){
 				mousepath = [];
+
+				var rect = canvas.getBoundingClientRect();
+				x = event.clientX - rect.left;
+				y = event.clientY - rect.top;
+
+				if (event.button === 0)
+				{
+					mouse_down = true;
+					paint(x,y);
+				}
+
+				if (event.button === 2)
+				{
+					// Sample color from canvas
+					id = context.getImageData(x,y,1,1).data;
+					brush_color = "#"+
+						float_to_hex(id[0]/255.0)+
+						float_to_hex(id[1]/255.0)+
+						float_to_hex(id[2]/255.0)
+				}
 			}
 
-			canvas.onmouseup = function(event){
-				mouse_down = false;
+			document.body.onmouseup = function(event){
+				if (event.button === 0)
+					mouse_down = false;
+			}
+
+			document.body.onmousemove = function(event){
+
+				// If there are no buttons pressed when the mouse
+				// is moveing, ensure that mouse_down is set to false
+				// so we dont accidentally draw lines across the canvas
+				if (event.buttons === 0)
+				{
+					mousepath = [];
+					mouse_down = false;
+				}
+			}
+
+
+			canvas.oncontextmenu = function(event){
+				// Disable rightclick on canvas
+				event.preventDefault();
 			}
 
 			canvas.onmousemove = function(event){
@@ -112,15 +182,29 @@ var painter = (function(){ // Namespace painter
 			toolbox.className = "toolbox";
 			container.appendChild(toolbox);
 
+			color_picker = document.createElement("div");
+			color_picker.className = "toolbox_color_picker";
+			toolbox.appendChild(color_picker);
+			Object.keys(bob_ross_colors).forEach(function(key){
+				color_button = document.createElement("button");
+				color_button.className = "color_picker";
+				color_button.style.background = bob_ross_colors[key];
+				color_button.onclick = function() {
+					brush_color = bob_ross_colors[key];
+				}
+				color_picker.appendChild(color_button);
+			});
+
+			brush_size_indicator = document.createElement("div");
+			brush_size_indicator.className = "toolbox_label";
+			toolbox.appendChild(brush_size_indicator);
+
 			brush_size_slider = document.createElement("input");
 			brush_size_slider.className = "toolbox_slider";
 			brush_size_slider.type = "range";
 			brush_size_slider.min = 0;
 			brush_size_slider.max = 1000;
 			toolbox.appendChild(brush_size_slider);
-			
-			brush_size_indicator = document.createElement("div");
-			toolbox.appendChild(brush_size_indicator);
 
 			brush_size_slider.oninput = function(event){
 				bs = brush_size_slider.value;
@@ -131,7 +215,30 @@ var painter = (function(){ // Namespace painter
 				brush_size = bs;
 				brush_size_indicator.innerHTML = "Brush size: "+bs+" px";
 			}
-			brush_size_slider.value = 10;
+
+			brush_size_slider.value = 500;
+			brush_size_slider.oninput();
+			toolbox.appendChild(document.createElement("br"));
+
+			brush_opacity_indicator = document.createElement("div");
+			brush_opacity_indicator.className = "toolbox_label";
+			toolbox.appendChild(brush_opacity_indicator);
+
+			brush_opacity_slider = document.createElement("input");
+			brush_opacity_slider.className = "toolbox_slider";
+			brush_opacity_slider.type = "range";
+			brush_opacity_slider.min = 0;
+			brush_opacity_slider.max = 1000;
+			toolbox.appendChild(brush_opacity_slider);
+
+			brush_opacity_slider.oninput = function(event){
+				bo = brush_opacity_slider.value / 1000;
+				brush_opacity = bo;
+				brush_opacity_indicator.innerHTML = "Brush opacity: "+Math.round(bo*100)+"%";
+			}
+
+			brush_opacity_slider.value = 100;
+			brush_opacity_slider.oninput();
 		}
 	}
 
